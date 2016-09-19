036ba3549b72c0e4e9b48d9860c09d8cfa14ddb4
Espen Klem <espen.klem@gmail.com>
2016-09-19 08:49:36 -0400

    Moving javascript to norch.js

--- norch.js
+++ norch.js
@@ -1,1 +1,44 @@
 // setup fielded search, filters, limits, etc.
+var vm = new Vue({
+    el: '#app',
+    data: {
+        searchresult: [],
+        queryinput: 'kake'
+    },
+    ready: function() {
+      this.search();
+    },
+    methods: {
+        search: function() {
+
+            var queryinput = this.queryinput.trim().toLowerCase();
+            var queryinput = queryinput.split(" ");
+            console.log('queryinput undef: ' + queryinput);
+
+            // query definition
+            var q = {};
+            q['query'] = [{'AND': [{'*': queryinput }]}];
+            q['categories'] = [{field: 'ingredients', limit: 10}];
+            q['pageSize'] = 20;
+            console.log('q object: ' + JSON.stringify(q, null, 4));
+            // JSON stringify query object
+            q = JSON.stringify(q);
+            console.log('Stringify q: ' + JSON.stringify(q, null, 4));
+            // URI encode query object
+            q = encodeURIComponent(q);
+            console.log('URIencode q: ' + JSON.stringify(q, null, 4));
+            var url = 'http://oppskrift.klemespen.com:3030/';
+            var endpoint = 'search?q=';
+
+            // GET request
+            this.$http.get(url + endpoint + q, function (data) {
+            //this.$http.get('http://oppskrift.klemespen.com:3030/search?q=%7B%22query%22%3A%7B%22AND%22%3A%5B%7B%22*%22%3A%5B%22' + queryinput + '%22%5D%7D%5D%7D%7D', function (data) {
+                // set data on vm
+                this.$set('searchresult', data);
+                //console.log(data);
+            }).catch(function (data, status, request) {
+                // handle error
+            })
+        }
+    }
+})
