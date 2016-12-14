9ecbbbdb3e201f5aa19b8b3ff79723d556ea840a
Espen Klem <espen.klem@gmail.com>
2016-12-14 05:59:40 -0500

    Proof of concept, fetching stuff from Norch and setting (not yet) to data model

---
+++ fetch.js
@@ -0,0 +1,27 @@
+// fetch() returns a promise that
+// resolves once headers have been received
+var data = ''
+
+fetch('http://oppskrift.klemespen.com:3030/search?q=%7B%22query%22%3A%7B%22AND%22%3A%7B%22Varenavn%22%3A%5B%22champagne%22%5D%7D%7D%7D')
+.then(response => {
+  // response.body is a readable stream.
+  // Calling getReader() gives us exclusive access to
+  // the stream's content
+
+  var decoder = new TextDecoder()
+  var reader = response.body.getReader()
+
+  // read() returns a promise that resolves
+  // when a value has been received
+  reader.read().then(function processResult(result) {
+    if (result.done) {
+      console.log(data)
+      return
+      // ### Here you set the data in the data model in VUE ###
+    }
+    data += decoder.decode(result.value, {stream: true})
+
+    // Read some more, and recall this function
+    return reader.read().then(processResult)
+  })
+})
